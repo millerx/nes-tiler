16df5714cc7c00422f9a9a166c097904b1131fac
Chris Miller <millerx@gmail.com>
2017-07-03 15:39:11 -0400

    Initial commit.

---
+++ renderer.js
@@ -0,0 +1,34 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const {ipcRenderer} = require('electron')
+
+function drawGrayscaleBytes(canvas, rom) {
+  var ctx = canvas.getContext('2d')
+
+  var id = ctx.createImageData(canvas.width, canvas.height)
+  for (var i = 0; i < rom.length; ++i) {
+    var di = i * 4
+    id.data[di++] = rom[i]
+    id.data[di++] = rom[i]
+    id.data[di++] = rom[i]
+    id.data[di++] = 255
+  }
+  ctx.putImageData(id, 0, 0)
+}
+
+function drawRom(rom) {
+  var canvas = document.getElementById('romCanvas')
+  // Resize the height of the canvas based on the size of the rom.
+  canvas.height = rom.length / canvas.width
+  drawGrayscaleBytes(canvas, rom)
+}
+
+/**
+ * Called when a ROM is loaded.
+ * rom  Buffer containing rom data.
+ */
+ipcRenderer.on('rom-loaded', (event, rom) => {
+  drawRom(rom)
+})
---
+++ rom.js
@@ -0,0 +1,77 @@
+/**
+ * Functions to work with NES ROMs.
+ */
+
+const fs = require('fs')
+
+const INesConsts = {
+  HEADER_SIZE: 16,
+
+  // Flags6
+  FLAGS6_HORIZONTAL_MIRRORING: 0x01,
+  FLAGS6_BATTERY_RAM: 0x02,
+  FLAGS6_TRAINER: 0x04,
+  FLAGS6_FOUR_SCREEN_VRAM: 0x08,
+  // Flags7
+  FLAGS7_VS_SYSTEM_CART: 0x01,
+  // Flags9
+  FLASG9_PAL_CART: 0x01,
+}
+
+/**
+ * Creates an iNes header object from the given buffer.
+ * Returns: {
+ *   prgRomSize:  // Size of PRG ROM in 16 KB units
+ *   chrRomSize:  // Size of CHR ROM in 8 KB units
+ *   prgRamSize:  // Size of PRG RAM in 8 KB units (Value 0 infers 8 KB for compatibility)
+ *   flags6:   // Flags for byte 6.
+ *   flags7:   // Flags for byte 7.  
+ *   flags9:   // Flags for byte 9.
+ *   flags10:  // Flags for byte 10 (unofficial).
+ *   mapper:   // Memory mapper used by the cartridge.  See http://fms.komkon.org/EMUL8/NES.html#LABM
+ * }
+ */
+function readINesHeader(buffer) {
+  var i = 0
+  // An iNES header starts with NES^Z (4e 45 53 1a)
+  if (buffer[i++] === 0x4e &&
+      buffer[i++] === 0x45 &&
+      buffer[i++] === 0x53 &&
+      buffer[i++] === 0x1a) {
+    var ines = {
+      prgRomSize: buffer[i++],
+      chrRomSize: buffer[i++],
+      flags6: buffer[i++],
+      flags7: buffer[i++],
+      prgRamSize: buffer[i++],
+      flags9: buffer[i++],
+      flags10: buffer[i++],
+    }
+    // flags6 bit 4-7 has the lower 4 bits of the mapper.
+    // flags7 bit 4-7 has the higher 4 bits of the mapper.
+    ines.mapper = (ines.flags6 >> 4) + (ines.flags7 & 0xF0)
+    return ines
+  } else {
+    // Not an iNES header.
+    return null
+  }
+}
+
+/**
+ * Synchronously reads a ROM file.
+ * Returns: {
+ *   inesHeader: {},  // Object with iNES structure read from the file.  null if iNES header is not recogised.
+ *   rom: Buffer      // Buffer containing the ROM including the iNES header.
+ *   rom_no_header: Buffer  // Buffer containing the ROM excluding the iNES header. 
+ * }
+ */
+exports.readRom = function(file) {
+  var buffer = fs.readFileSync(file)
+  var inesHeader = readINesHeader(buffer)
+  return {
+    inesHeader: inesHeader,
+    rom: buffer,
+    // slice() is a light weight operation.
+    rom_no_header: inesHeader ? buffer.slice(INesConsts.HEADER_SIZE) : buffer
+  }
+}
